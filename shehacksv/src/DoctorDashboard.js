a5936e2188bc172be664e9b3371128f76a2494ae
Netra2104 <42544158+Netra2104@users.noreply.github.com>
2021-01-10 08:00:16 -0500

    txt
    wefwe

--- shehacksv/src/DoctorDashboard.js
+++ shehacksv/src/DoctorDashboard.js
@@ -592,6 +592,54 @@ class DoctorDashboard extends React.Component {
                 </div>
                 </div>
               </div>
+              <div className="row">
+              <div class="col-xl-4 col-lg-5">
+                  <div class="card shadow mb-4">
+                    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                      <h6 class="m-0 font-weight-bold text-primary">
+                        Doctor's Notes
+                      </h6>
+                      <div class="dropdown no-arrow">
+                        <a
+                          class="dropdown-toggle"
+                          href="#"
+                          role="button"
+                          id="dropdownMenuLink"
+                          data-toggle="dropdown"
+                          aria-haspopup="true"
+                          aria-expanded="false"
+                        >
+                          <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                        </a>
+                        <div
+                          class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                          aria-labelledby="dropdownMenuLink"
+                        >
+                          <div class="dropdown-header">Dropdown Header:</div>
+                          <a class="dropdown-item" href="#">
+                            Action
+                          </a>
+                          <a class="dropdown-item" href="#">
+                            Another action
+                          </a>
+                          <div class="dropdown-divider"></div>
+                          <a class="dropdown-item" href="#">
+                            Something else here
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div class="card-body">
+                    
+                    <div class="form-group">
+    <label for="exampleFormControlTextarea1">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
