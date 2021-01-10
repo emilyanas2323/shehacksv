a3c98cd418a26cc0b5f404c20306d2c2ff539487
Netra2104 <42544158+Netra2104@users.noreply.github.com>
2021-01-09 20:02:44 -0500

    Added React Routing for App.js
    Make sure you do npm install again

--- shehacksv/src/App.js
+++ shehacksv/src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
-import PatientDashboard from './PatientDashboard';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import PatientDashboard from "./PatientDashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PatientSignUp from "./PatientSignUp";
+import MainPage from "./MainPage";
+import DoctorSignUp from "./DoctorSignUp";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <PatientDashboard></PatientDashboard>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/patientsignup" component={PatientSignUp} />
+          <Route exact path="/doctorsignup" component={DoctorSignUp} />
+          <Route exact path="/patientdashboard" component={PatientDashboard} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
