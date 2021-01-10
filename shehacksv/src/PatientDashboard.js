1699e0d26f49aab91d68570bbcfecee093f344bc
Sofya Pryadko <spryadko@uwo.ca>
2021-01-10 04:14:17 -0500

    sofya

--- shehacksv/src/PatientDashboard.js
+++ shehacksv/src/PatientDashboard.js
@@ -10,15 +10,26 @@ class PatientDashboard extends React.Component {
 
     this.onSelect = this.onSelect.bind(this);
     this.onRemove = this.onRemove.bind(this);
+    this.onSelectIssue = this.onSelectIssue.bind(this);
+    this.onRemoveIssue = this.onRemoveIssue.bind(this);
     this.onProposedSelect = this.onProposedSelect.bind(this);
     this.onProposedRemove = this.onProposedRemove.bind(this);
+    this.onProposedSelectIssue = this.onProposedSelectIssue.bind(this);
+    this.onProposedRemoveIssue = this.onProposedRemoveIssue.bind(this);
     this.additionalSymptoms = this.additionalSymptoms.bind(this);
+    this.additionalIssues = this.additionalIssues.bind(this);
     this.updateProposedSymptoms = this.updateProposedSymptoms.bind(this);
+    this.updateInfoIssues = this.updateInfoIssues.bind(this);
+
 
     this.state = {
+      issueid: 0,
       options: [],
+      optionsIssues: [],
       selectedSymptoms: [],
       proposedSymptoms: [],
+      selectedIssues: [],
+      proposedIssues: [],
       firstName: "",
       lastName: "",
       DOB: new Date(),
@@ -32,23 +43,46 @@ class PatientDashboard extends React.Component {
       url: "https://priaid-symptom-checker-v1.p.rapidapi.com/symptoms",
       params: { language: "en-gb", format: "json" },
       headers: {
-        "x-rapidapi-key": "71491ac6a2msh7ddcf0ac4b9fab3p189889jsn5cf483e878e0",
+        "x-rapidapi-key": "a4dc6a92bemshd411cecf19a1136p197d8ejsnace6d7bd4d5e",
         "x-rapidapi-host": "priaid-symptom-checker-v1.p.rapidapi.com",
       },
     };
 
-    // axios request
-    // axios
-    //   .request(options)
-    //   .then((response) => {
-    //     console.log(response.data);
-    //     this.setState({
-    //       options: response.data,
-    //     });
-    //   })
-    //   .catch(function (error) {
-    //     console.error(error);
-    //   });
+    const optionsIssues = {
+      method: "GET",
+      url: "https://priaid-symptom-checker-v1.p.rapidapi.com/issues",
+      params: { language: "en-gb", format: "json" },
+      headers: {
+        "x-rapidapi-key": "a4dc6a92bemshd411cecf19a1136p197d8ejsnace6d7bd4d5e",
+        "x-rapidapi-host": "priaid-symptom-checker-v1.p.rapidapi.com",
+      },
+    };
+
+    //axios request ISSUES
+     axios
+      .request(optionsIssues)
+       .then((response) => {
+       console.log(response.data);
+       this.setState({
+          optionsIssues: response.data,
+         });
+       })
+       .catch(function (error) {
+         console.error(error);
+       });
+
+     //axios request
+     axios
+      .request(options)
+       .then((response) => {
+       console.log(response.data);
+       this.setState({
+          options: response.data,
+         });
+       })
+       .catch(function (error) {
+         console.error(error);
+       });
 
     // using sample data
     /*this.setState({
@@ -94,30 +128,69 @@ class PatientDashboard extends React.Component {
         symptoms: symptoms,
       },
       headers: {
-        "x-rapidapi-key": "71491ac6a2msh7ddcf0ac4b9fab3p189889jsn5cf483e878e0",
+        "x-rapidapi-key": "a4dc6a92bemshd411cecf19a1136p197d8ejsnace6d7bd4d5e",
         "x-rapidapi-host": "priaid-symptom-checker-v1.p.rapidapi.com",
       },
     };
 
     console.log(options.params.year_of_birth);
 
+    
     // axios request
-    // axios
-    //   .request(options)
-    //   .then((response) => {
-    //     console.log(response.data);
-    //     this.setState({
-    //       proposedSymptoms: response.data,
-    //     });
-    //   })
-    //   .catch(function (error) {
-    //     console.error(error);
-    //   });
-
+     axios
+     .request(options)
+     .then((response) => {
+        console.log(response.data);
+       this.setState({
+         proposedSymptoms: response.data,
+        });
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+      
     /*this.setState({
         proposedSymptoms: proposed
     })*/
   }
+
+    updateInfoIssues(selectedIssues) {
+    let issueid = 0;
+    selectedIssues.forEach((item) => {
+      issueid += item.ID;
+    });
+    console.log(issueid); //works
+    const optionsIssues = {
+      method: "GET",
+      url: "https://priaid-symptom-checker-v1.p.rapidapi.com/issues/11/info",
+      params: {
+        language: "en-gb",
+        issue_id: issueid,
+      },
+      headers: {
+        "x-rapidapi-key": "a4dc6a92bemshd411cecf19a1136p197d8ejsnace6d7bd4d5e",
+        "x-rapidapi-host": "priaid-symptom-checker-v1.p.rapidapi.com",
+      },
+    };
+          // axios request
+          axios
+     .request(optionsIssues)
+     .then((response) => { //response becomes text but should be array for multiselect
+        let sym = response.data.PossibleSymptoms.split(",");
+        for (let i = 0; i<sym.length; i++){
+            sym[i] = {Name: sym[i]};
+        }
+       this.setState({
+         proposedIssues: sym,
+        });
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+
+
+
 
   onSelect(selectedList, selectedItem) {
     let symptoms = this.state.selectedSymptoms;
@@ -161,6 +234,49 @@ class PatientDashboard extends React.Component {
     console.log(symptoms);
   }
 
+  onSelectIssue(selectedList, selectedItem) {
+    let issues = this.state.selectedIssues;
+    issues.push(selectedItem);
+    console.log(issues);
+    this.setState({
+      selectedIssues: issues,
+    });
+    console.log(issues);
+    this.updateInfoIssues(issues);
+  }
+
+  onRemoveIssue(selectedList, removedItem) {
+    let issues = this.state.selectedIssues;
+    issues = issues.filter(function (obj) {
+      return obj.Name !== removedItem.Name;
+    });
+    this.setState({
+      selectedIssues: issues,
+    });
+    console.log(issues);
+    this.updateInfoIssues(issues);
+  }
+
+  onProposedSelectIssue(selectedList, selectedItem) {
+    let issues = this.state.proposedIssues;
+    issues.push(selectedItem);
+    this.setState({
+      proposedIssues: issues,
+    });
+    console.log(issues);
+  }
+
+  onProposedRemoveIssue(selectedList, removedItem) {
+    let issues = this.state.proposedIssues;
+    issues = issues.filter(function (obj) {
+      return obj.Name !== removedItem.Name;
+    });
+    this.setState({
+      proposedIssues: issues,
+    });
+    console.log(issues);
+  }
+
   additionalSymptoms() {
     if (this.state.selectedSymptoms.length > 0) {
       return (
@@ -177,6 +293,23 @@ class PatientDashboard extends React.Component {
     } else return null;
   }
 
+  additionalIssues() {
+    if (this.state.selectedIssues.length > 0) {
+      return (
+        <div>
+          <p>Are you also experiencing any the below symptoms?</p>
+          <Multiselect
+            options={this.state.proposedIssues} // Options to display in the dropdown
+            onSelect={this.onProposedSelectIssue} // Function will trigger on select event
+            onRemove={this.onProposedRemoveIssue} // Function will trigger on remove event
+            displayValue="Name" // Property name to display in the dropdown options
+          />
+        </div>
+      );
+    } else return null;
+  }
+
+
   render() {
     return (
       <div>
@@ -191,8 +324,18 @@ class PatientDashboard extends React.Component {
           displayValue="Name" // Property name to display in the dropdown options
         />
         {this.additionalSymptoms()}
-        <div className="btn btn-primary">Submit</div>
+        <h3>Search by issues</h3>
+        <p>Please select the issues you are experiencing:</p>
+        <Multiselect
+          options={this.state.optionsIssues} // Options to display in the dropdown
+          onSelect={this.onSelectIssue} // Function will trigger on select event
+          onRemove={this.onRemoveIssue} // Function will trigger on remove event
+          displayValue="Name" // Property name to display in the dropdown options
+        />
+        {this.additionalIssues()}
+      <div className="btn btn-primary">Submit</div>
       </div>
+
     );
   }
 }
